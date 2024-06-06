acc425b265aa1e3fc9f5f8cf740efc845e788ffe
adrien2p <adrien.deperetti@gmail.com>
2024-06-06 12:44:26 +0200

    update config

--- backend/medusa-config.js
+++ backend/medusa-config.js
@@ -1,26 +1,7 @@
-const dotenv = require("dotenv");
 const { Modules } = require("@medusajs/modules-sdk");
+const { defineConfig, loadEnv } = require("@medusajs/utils");
 
-let ENV_FILE_NAME = "";
-switch (process.env.NODE_ENV) {
-  case "production":
-    ENV_FILE_NAME = ".env.production";
-    break;
-  case "staging":
-    ENV_FILE_NAME = ".env.staging";
-    break;
-  case "test":
-    ENV_FILE_NAME = ".env.test";
-    break;
-  case "development":
-  default:
-    ENV_FILE_NAME = ".env";
-    break;
-}
-
-try {
-  dotenv.config({ path: process.cwd() + "/" + ENV_FILE_NAME });
-} catch (e) {}
+loadEnv(process.env.NODE_ENV || "development", process.cwd())
 
 // CORS when consuming Medusa from admin
 // Medusa's docs are added for a better learning experience. Feel free to remove.
@@ -43,97 +24,63 @@ const DATABASE_URL =
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
-const plugins = [];
-
-const modules = {
-  restaurantModuleService: {
-    resolve: "./modules/restaurant",
-  },
-  deliveryModuleService: {
-    resolve: "./modules/delivery",
-  },
-  [Modules.CACHE]: true,
-  [Modules.EVENT_BUS]: true,
-  [Modules.AUTH]: true,
-  [Modules.USER]: {
-    resolve: "@medusajs/user",
-    options: {
-      jwt_secret: process.env.JWT_SECRET ?? "supersecret",
-    },
-  },
-  [Modules.FILE]: {
-    resolve: "@medusajs/file",
-    options: {
-      providers: [
-        {
-          resolve: "@medusajs/file-local-next",
-          options: {
-            config: {
-              local: {},
-            },
-          },
-        },
-      ],
-    },
-  },
-  [Modules.WORKFLOW_ENGINE]: true,
-  [Modules.STOCK_LOCATION]: true,
-  [Modules.INVENTORY]: true,
-  [Modules.PRODUCT]: true,
-  [Modules.PRICING]: true,
-  [Modules.PROMOTION]: true,
-  [Modules.CUSTOMER]: true,
-  [Modules.SALES_CHANNEL]: true,
-  [Modules.CART]: true,
-  [Modules.REGION]: true,
-  [Modules.API_KEY]: true,
-  [Modules.STORE]: true,
-  [Modules.TAX]: true,
-  [Modules.CURRENCY]: true,
-  [Modules.PAYMENT]: true,
-  [Modules.ORDER]: true,
-  [Modules.FULFILLMENT]: {
-    resolve: "@medusajs/fulfillment",
-    options: {
-      providers: [
-        {
-          resolve: "@medusajs/fulfillment-manual",
-          options: {
-            config: {
-              manual: {},
-            },
-          },
-        },
-      ],
-    },
-  },
-};
-
-/** @type {import('@medusajs/medusa').ConfigModule["projectConfig"]} */
-const projectConfig = {
-  databaseUrl: DATABASE_URL,
-  http: {
-    storeCors: STORE_CORS,
-    adminCors: ADMIN_CORS,
-    authCors: process.env.AUTH_CORS || ADMIN_CORS,
-    jwtSecret: process.env.JWT_SECRET || "supersecret",
-    cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-  },
-  redis_url: REDIS_URL,
-};
-
-/** @type {import('@medusajs/types').ConfigModule} */
-module.exports = {
-  projectConfig,
+export default defineConfig({
   admin: {
     backendUrl: "http://localhost:9000",
   },
-  plugins,
-  modules,
-  featureFlags: {
-    medusa_v2: true,
+  projectConfig: {
+    databaseUrl: DATABASE_URL,
+    http: {
+      storeCors: STORE_CORS,
+      adminCors: ADMIN_CORS,
+      authCors: process.env.AUTH_CORS || ADMIN_CORS,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    },
+    redisUrl: REDIS_URL,
   },
-  directories: {
-    srcDir: "src",
-  },
-};
+  modules: {
+    [Modules.USER]: {
+      resolve: "@medusajs/user",
+      options: {
+        jwt_secret: process.env.JWT_SECRET ?? "supersecret",
+      },
+    },
+    [Modules.FILE]: {
+      resolve: "@medusajs/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/file-local-next",
+            options: {
+              config: {
+                local: {},
+              },
+            },
+          },
+        ],
+      },
+    },
+    [Modules.FULFILLMENT]: {
+      resolve: "@medusajs/fulfillment",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/fulfillment-manual",
+            options: {
+              config: {
+                manual: {},
+              },
+            },
+          },
+        ],
+      },
+    },
+    restaurantModuleService: {
+      resolve: "./modules/restaurant",
+    },
+    deliveryModuleService: {
+      resolve: "./modules/delivery",
+    },
+  }
+})
