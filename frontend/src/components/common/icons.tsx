ab7e85271af75b3a7b68b1c03792eb2a7e55613a
Victor Gerbrands <victorgerbrands@gmail.com>
2024-08-12 15:43:26 +0200

    fix: icons displayname

--- frontend/src/components/common/icons.tsx
+++ frontend/src/components/common/icons.tsx
@@ -1,5 +1,4 @@
 import { Bike, Pizza, Store } from "lucide-react";
-
 import React, {
   ForwardRefExoticComponent,
   PropsWithChildren,
@@ -11,14 +10,17 @@ export const PizzaIcon: ForwardRefExoticComponent<
 > = React.forwardRef<SVGSVGElement>((props: PropsWithChildren, ref) => (
   <Pizza ref={ref} size={16} {...props} />
 ));
+PizzaIcon.displayName = "PizzaIcon";
 
 export const BikeIcon: ForwardRefExoticComponent<RefAttributes<SVGSVGElement>> =
   React.forwardRef<SVGSVGElement>((props: PropsWithChildren, ref) => (
     <Bike ref={ref} size={16} {...props} />
   ));
+BikeIcon.displayName = "BikeIcon";
 
 export const StoreIcon: ForwardRefExoticComponent<
   RefAttributes<SVGSVGElement>
 > = React.forwardRef<SVGSVGElement>((props: PropsWithChildren, ref) => (
   <Store ref={ref} size={16} {...props} />
 ));
+StoreIcon.displayName = "StoreIcon";
